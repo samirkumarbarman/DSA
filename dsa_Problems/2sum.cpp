347013be5fbfbca668f9e2b7cc76c99834b8938e
Samir kumar barman <91568013+samirkumarbarman@users.noreply.github.com>
2025-02-14 23:48:44 +0600

    Create 2sum.cpp

---
+++ dsa_Problems/2sum.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+vector<int> twoSum(vector<int>& nums, int target) {
+    unordered_map<int, int> numMap;
+
+    for (int i = 0; i < nums.size(); ++i) {
+        int complement = target - nums[i];
+        if (numMap.count(complement)) {
+            return {numMap[complement], i};
+        }
+        numMap[nums[i]] = i;
+    }
+
+    return {};
+}
+
+int main() {
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+
+    vector<int> result = twoSum(nums, target);
+
+    if (!result.empty()) {
+        cout << "Indices: " << result[0] << ", " << result[1] << endl;
+    } else {
+        cout << "No two sum solution found." << endl;
+    }
+
+    nums = {3,2,4};
+    target = 6;
+
+    result = twoSum(nums, target);
+
+    if (!result.empty()) {
+        cout << "Indices: " << result[0] << ", " << result[1] << endl;
+    } else {
+        cout << "No two sum solution found." << endl;
+    }
+
+    nums = {3,3};
+    target = 6;
+
+    result = twoSum(nums, target);
+
+    if (!result.empty()) {
+        cout << "Indices: " << result[0] << ", " << result[1] << endl;
+    } else {
+        cout << "No two sum solution found." << endl;
+    }
+
+    return 0;
+}
